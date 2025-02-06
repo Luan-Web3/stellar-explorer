5949b755937b305bdd32660a3d5387f192b48eb3
Luan-Web3 <luansilvalemes.web3@gmail.com>
2025-02-06 00:14:28 -0300

    feat: add docker-compose.yml

--- frontend/src/components/TransactionDetails.jsx
+++ frontend/src/components/TransactionDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Typography, Container, Grid, Paper } from '@mui/material';
-import stellarService from '../services/stellarService';
 import api from '../axios';
 
 function TransactionDetails() {
--- frontend/src/components/TransactionList.jsx
+++ frontend/src/components/TransactionList.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { List, ListItem, ListItemText, Divider, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import stellarService from '../services/stellarService';
 import api from '../axios';
 
 function TransactionList() {
